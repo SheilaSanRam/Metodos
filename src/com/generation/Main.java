39ab1a59e7b3725206e3d86c1635d0c6fcbdc057
Sheila Sanchez Ramirez <sanchezramirezsheila@gmail.com>
2021-10-20 17:06:26 -0500

    Métodos en Java

---
+++ src/com/generation/Main.java
@@ -0,0 +1,47 @@
+package com.generation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Métodos (funciones)
+        //llamado a la función
+//        String mensaje=saludar("Sheila");
+//        System.out.println(mensaje);
+//        int resultado=suma(5,3);
+//        System.out.println(resultado);
+//        alerta("Mensaje de prueba");
+//
+//        System.out.println(Math.PI);
+//        double num1=13.75;
+//        double num2=23.12;
+//
+//        System.out.println(Math.sqrt(num2));
+
+        String mensaje1="hola";
+        String mensaje2="Hola";
+        //tamaño de la cadena
+        System.out.println(mensaje2.length());
+        //compara dos cadenas
+        System.out.println(mensaje1.equals(mensaje2));
+        //compara dos cadenas ignorando si son mayúsculas o minúsculas
+        System.out.println(mensaje1.equalsIgnoreCase(mensaje2));
+    }
+//
+//    //Este es nu nuevo método
+//    //Se declara que es la devolución de la función (return) y el nombre de la función.
+//
+//    public static String saludar(String nombre){
+//
+//        return "¡hola, "+nombre+"! ¿Cómo estás?";
+//    }
+//
+//    public static int suma(int numero1, int numero2){
+//        return numero1+numero2;
+//    }
+//
+//    public static void alerta(String mensaje){
+//        System.out.println(mensaje);
+//    }
+
+
+}
